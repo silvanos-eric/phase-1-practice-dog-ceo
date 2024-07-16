49f03ba427807c8950ec209ad839a3c4a2674b55
Silvanos Eric <ericoderito@gmail.com>
2024-07-16 07:51:15 +0300

    feat: enhance initial setup with styling and dynamic image loading
    - Add `style.css` to reset default margins and paddings for consistent styling.
    - Modify `index.html`:
      - Add `defer` attribute to script tag for better loading performance.
      - Add link to external stylesheet.
      - Cleanup and format code.
    - Update `index.js`:
      - Implement dynamic loading of dog images from API.
      - Append images to the DOM after fetching.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+  // Challenge 1
+  const dogImageContainerEl = document.querySelector("div#dog-image-container");
+  const imagesFragment = document.createDocumentFragment();
+
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then(({ message }) => {
+      message.forEach((img) => {
+        const newImgEl = document.createElement("img");
+        newImgEl.setAttribute("src", img);
+        newImgEl.classList.add("dog-image");
+
+        imagesFragment.appendChild(newImgEl);
+      });
+    })
+    .then(() => {
+      dogImageContainerEl.appendChild(imagesFragment);
+    });
+});
